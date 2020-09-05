218b675820a11555339537e4c85af0b74b5e8ee2
wfedev <wfedev@gamil.com>
2020-09-05 11:03:06 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.695dd3fe9a17e315d523794b45c683b7.js"
+  "/precache-manifest.c8442e16d1f5dce18b05426b1d497791.js"
 );
 
 self.addEventListener('message', (event) => {
